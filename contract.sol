8fd2e03110052fc1acad565c615402ce4bf3006a
Alan Schambers <metabou@alans-mbp.attlocal.net>
2017-12-24 04:45:51 -0800

    first commit

---
+++ contract.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.18;
+
+contract course {
+	string fname;
+	uint age;
+
+	function setInstructor(string _fname, uint _age) public {
+		fname = _fname;
+		age = _age;
+	}
+
+	function getInstructor() public constant returns(string, uint) {
+		return (fname, age);
+	}
+}
