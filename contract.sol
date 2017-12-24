ce0123680935f0a5e2a64ee7936aaf2d0355b938
Alan Schambers <metabou@alans-mbp.attlocal.net>
2017-12-24 05:55:04 -0800

    add onlyOwner modifier to make it so only the owner of the contract can add an instructor

--- contract.sol
+++ contract.sol
@@ -3,13 +3,24 @@ pragma solidity ^0.4.18;
 contract course {
 	string fname;
 	uint age;
+	address owner;
+	
+	function course() public {
+	    owner = msg.sender;
+	}
+	
+	modifier onlyOwner {
+	    require(msg.sender == owner);
+	    // if true, run the function body in place of _;
+	    _;
+	}
 
 	event Instructor(
 		string name,
 		uint age
 	);
 
-	function setInstructor(string _fname, uint _age) public {
+	function setInstructor(string _fname, uint _age) onlyOwner public {
 		fname = _fname;
 		age = _age;
 		Instructor(_fname, _age);
