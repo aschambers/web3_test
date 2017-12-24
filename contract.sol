604986d3e12981bc1cb8e4e75e109e75d683c35b
Alan Schambers <metabou@alans-mbp.attlocal.net>
2017-12-24 05:35:42 -0800

    add event to solidity contract, and work with event in browser using web3

--- contract.sol
+++ contract.sol
@@ -4,9 +4,15 @@ contract course {
 	string fname;
 	uint age;
 
+	event Instructor(
+		string name,
+		uint age
+	);
+
 	function setInstructor(string _fname, uint _age) public {
 		fname = _fname;
 		age = _age;
+		Instructor(_fname, _age);
 	}
 
 	function getInstructor() public constant returns(string, uint) {
